3109b4b959d40972652ec3dc711c2b587d71f696
AmanTiwari <er.amantiwari.2002@gmail.com>
2025-07-28 20:20:20 +0530

    Set up Model : Contacts

---
+++ models/Contacts.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const contactSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    company : {
+        type : String,
+    },
+    phone : {
+        type : String,
+        required : true,
+        unique: true,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    }
+},{
+    timestamps : true
+})
+const Contacts = mongoose.model('Contacts',contactSchema)
+export default Contacts
