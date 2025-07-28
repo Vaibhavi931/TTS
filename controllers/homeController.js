f32a18dd314f22f2a452128a5faaecfa3db2b77f
AmanTiwari <er.amantiwari.2002@gmail.com>
2025-07-28 19:42:48 +0530

    Set up Controllers structure

---
+++ controllers/homeController.js
@@ -0,0 +1,3 @@
+export const home = (req,res)=>{
+    return res.send("<h1>Server Running</h1>")
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 import express from "express";
+import { home } from "../controllers/homeController.js";
 const router = express.Router()
 
-router.get("/",(req,res)=>{
-    return res.send("<h1>Server Running</h1>")
-})
+router.get("/",home)
 
 export default router
