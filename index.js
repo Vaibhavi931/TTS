0ba8a651c5ee508fdb7d420e53509db43db88164
AmanTiwari <er.amantiwari.2002@gmail.com>
2025-07-28 19:36:12 +0530

    Setting Up Environment Varibales

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config()
+
 const app = express()
+const PORT = process.env.PORT || 8000
 
 app.get("/",(req,res)=>{
     return res.send("<h1>Server Running</h1>")
 })
 
-app.listen(8000,(err)=>{
+app.listen(PORT,(err)=>{
     if(err){
         console.log("ERROR OCCURRED",err);
         return 
     }
-    console.log("SERVER STARTED AT PORT ",8000);
+    console.log("SERVER STARTED AT PORT ",PORT);
 })
